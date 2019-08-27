fc0183881af943d2fa3ad18787993e7bc166d054
Obed Vega <30850480+ovvake@users.noreply.github.com>
2019-08-26 22:07:12 -0400

    Validacion de Usuario

---
+++ checkUser.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+include "dbconnection.php";
+
+$uname = $_POST['username'];  
+$password = $_POST['password'];
+
+if ($uname != "" && $password != ""){
+
+    $sql = "select count(*) as cntUser from usuarios where correo='".$uname."' and password='".$password."'";
+    $result = $conn->query($sql);
+    $row = mysqli_fetch_array($result);
+
+    $count = $row['cntUser'];
+
+    if($count > 0){
+        $_SESSION['uname'] = $uname;
+        echo 1;
+    }else{
+        echo 0;
+    }
+
+}
+$conn->close(); 
+?>
